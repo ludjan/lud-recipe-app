fb4dda75241190775ccc6ce976c57098e9f30e5b
ludjan <ludviggjansson@gmail.com>
2022-07-22 00:12:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ var app = express() // use the express framework to handle dynamic responses to 
 app.use(express.json()) // make sure express parses bodies with json
 app.use(cors()) // make sure we can access the api from the outside
 
-
 var server = http.Server(app)
 
 const recipeList = [
     { id: 1, name: "Mat", taste: "digg", link: "recipes/1" },
     { id: 2, name: "Drikk", taste: "usch", link: "recipes/2" },
     { id: 3, name: "Hulda", taste: "nja", link: "recipes/3" }
-  ]
+]
+
+app.get('/', (req, res) => {
+  res.status(200).send('./public/index.html')
+})
 
 server.listen(port, function() {
     console.log(`Web server running on ${hostname} port ${port}`)
