8f532c67269ef1c441060551f4bbfbeda91b0459
ludjan <ludviggjansson@gmail.com>
2022-07-22 00:17:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,15 +11,20 @@ app.use(cors()) // make sure we can access the api from the outside
 
 var server = http.Server(app)
 
+// set view engine to ejs
+app.set("view engine", "ejs")
+
+
 const recipeList = [
     { id: 1, name: "Mat", taste: "digg", link: "recipes/1" },
     { id: 2, name: "Drikk", taste: "usch", link: "recipes/2" },
     { id: 3, name: "Hulda", taste: "nja", link: "recipes/3" }
 ]
 
-app.get('/', (req, res) => {
-  res.status(200).send('./public/index.html')
+app.get("/", (req, res) => {
+  res.render("recipeList", { data: recipeList })
 })
+
 
 server.listen(port, function() {
     console.log(`Web server running on ${hostname} port ${port}`)
