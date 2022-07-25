3a17d0b84840e339cf67c90a697d07fae9ee4187
ludjan <ludviggjansson@gmail.com>
2022-07-25 23:48:51 +0200

    Update recipe.js

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -9,62 +9,31 @@ async function updateRecipe(recipe, id) {
     },
     body: JSON.stringify(recipe)
   }
-
-  return response = await fetch(targetUrl, config)
+  return await fetch(targetUrl, config)
 }
 
 async function addRecipe(recipe) {
-
-  // fetch the post 
-  const response = await fetch(apiUrl, {
+  return await fetch(apiUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(recipe)
   })
-  return response
 }
 
 async function deleteRecipe(recipe) {
-
   const targetUrl = apiUrl + "/" + recipe.id
-
   const config = {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
     }
   }
-
   fetch(targetUrl, config)
     .then(response => response.json())
     .then(data => {
       console.log(data)
       alert(data)
     })
-    // .then(render())
 }
-
-// async function render() {
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data)
-//       feed.innerHTML = `<ul>`
-//       data.forEach(recipe => {
-//         const recipeUrl = url + '/' + recipe.id
-//         feed.innerHTML += `
-//         <li>
-//           <a href="${recipeUrl}">
-//             ${recipe.name}
-//           </a>
-//         </li>`
-//       })
-//       feed.innerHTML += `</ul>`
-//     })
-//     .catch(err => console.log(err))
-// }
-
-// render the page on first load
-// render()
